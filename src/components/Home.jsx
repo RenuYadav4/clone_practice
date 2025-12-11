b44fcf5c943d25c77657390510abed25d63c8378
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-12-11 14:51:49 +0530

    add br tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>This is the home page <span> from siwan</span> </div>
+    <div>This is the home page <span> from siwan</span> <br /> </div>
   )
 }
 
