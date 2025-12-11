4072318860a7a6363165f4bd4fc687c97e8e9543
Renu Yadav <renuya886@gmail.com>
2025-12-11 15:09:26 +0530

    div has added successfully

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>This is the home page <span> from siwan</span> <br /> </div>
+    <>
+        <div>This is the home page <span> from siwan</span> <br /> </div>
+        <div>adding the second Div to Home component.</div>
+    </>
   )
 }
 
